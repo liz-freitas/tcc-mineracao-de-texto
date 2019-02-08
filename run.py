961b98c0ca1f91b20896ce43a630182bd0e9a5ee
liz-freitas <elizangela.freitas.2901@gmail.com>
2019-02-08 19:43:08 -0200

    Adding more classifiers to comparison

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 #Importação das bibliotecas
+import warnings
+warnings.filterwarnings(action='ignore', category=UserWarning, module='gensim')
+import gensim
 import random
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 from nltk.tokenize import word_tokenize
@@ -7,9 +10,12 @@ from nltk.corpus import stopwords
 import unicodedata
 import csv
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn import metrics
 # import tkinter
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -81,6 +87,7 @@ def treinar_modelo(tagged_data):
                     alpha=alpha, 
                     min_alpha=0.00025, # A taxa de aprendizado cairá linearmente para min_alpha à medida que o treinamento progride.
                     min_count=1,#Ignora todas as palavras com frequência total menor que o valor parametrizado.
+                    window = 20,
                     dm =1) #Define o algoritmo de treinamento. Se dm = 1 , 'memória distribuída' (PV-DM) é usada. Caso contrário, o pacote distribuído de palavras (PV-DBOW) é empregado.
 
     # Constroe o vocabulário
@@ -103,8 +110,9 @@ def treinar_modelo(tagged_data):
 def gerar_vetor(model, wordVector):
     return model.infer_vector(wordVector, steps=1000, alpha=0.01)
 
+# ---- fazer uma função para cada classificador a ser utlizado ----
 #Função que gera os classificadores 
-def gerar_classificador(model, suicidas, nao_suicidas):
+def gerar_classificador_reg_logistica(model, suicidas, nao_suicidas):
     # Mesclando tabelas de validação
     array =  [[gerar_vetor(model, suicida), 1] for suicida in suicidas]
     array += [[gerar_vetor(model, nao_suicida), 0] for nao_suicida in nao_suicidas]
@@ -126,7 +134,77 @@ def gerar_classificador(model, suicidas, nao_suicidas):
 
     return classificador
 
-#Função que mede a precisão
+#Função que gera os classificadores (Random Forest)
+def gerar_classificador_random_forest(model, suicidas, nao_suicidas):
+    # Mesclando tabelas de validação
+    array =  [[gerar_vetor(model, suicida), 1] for suicida in suicidas]
+    array += [[gerar_vetor(model, nao_suicida), 0] for nao_suicida in nao_suicidas]
+
+    # Randomizando array
+    random.shuffle(array)
+
+    # Separando array de teste das labels
+    train_array = []
+    train_labels = []
+
+    for index in range(len(array)):
+        train_array.append(array[index][0])
+        train_labels.append(array[index][1])
+        
+    # Treinando regressão linear
+    classificador = RandomForestClassifier()
+    classificador.fit(train_array, train_labels)
+
+    return classificador
+
+
+#Função que gera os classificadores ( Naive Bayes¶)
+def gerar_classificador_naive_bayes(model, suicidas, nao_suicidas):
+    # Mesclando tabelas de validação
+    array =  [[gerar_vetor(model, suicida), 1] for suicida in suicidas]
+    array += [[gerar_vetor(model, nao_suicida), 0] for nao_suicida in nao_suicidas]
+
+    # Randomizando array
+    random.shuffle(array)
+
+    # Separando array de teste das labels
+    train_array = []
+    train_labels = []
+
+    for index in range(len(array)):
+        train_array.append(array[index][0])
+        train_labels.append(array[index][1])
+        
+    # Treinando Naive Bayes
+    classificador = GaussianNB()
+    classificador.fit(train_array, train_labels)
+
+    return classificador
+
+#Função que gera os classificadores ( Gradient Boosting)
+def gerar_classificador_gradient_boosting(model, suicidas, nao_suicidas):
+    # Mesclando tabelas de validação
+    array =  [[gerar_vetor(model, suicida), 1] for suicida in suicidas]
+    array += [[gerar_vetor(model, nao_suicida), 0] for nao_suicida in nao_suicidas]
+
+    # Randomizando array
+    random.shuffle(array)
+
+    # Separando array de teste das labels
+    train_array = []
+    train_labels = []
+
+    for index in range(len(array)):
+        train_array.append(array[index][0])
+        train_labels.append(array[index][1])
+
+    # Treinando Gradient Boosting
+    classificador = GradientBoostingClassifier()
+    classificador.fit(train_array, train_labels)
+
+    return classificador
+
+#Função que mede a precisão - REMOVER
 def aferir_precisao(model, classifier, suicidas, nao_suicidas):
     total = len(suicidas) + len(nao_suicidas)
     acertos = 0
@@ -195,6 +273,20 @@ def plot_ROC( nao_suicida, taxa_verdadeiro_positivo, auc ):
     plt.show()
     return plt
 
+def print_metrics(vetor_esperado, vetor_resultados):
+    precisao = metrics.precision_score(vetor_esperado, vetor_resultados)
+    print("Taxa de precisão utilizando:\n{:6.2f}%".format(precisao * 100))
+
+    acuracia = metrics.accuracy_score(vetor_esperado, vetor_resultados)
+    print("Taxa de acurácia utilizando:\n{:6.2f}%".format(acuracia * 100))
+
+    recall = metrics.recall_score(vetor_esperado, vetor_resultados)
+    print("Taxa de Recall utilizando:\n{:6.2f}%".format(recall * 100))
+
+    f1 = metrics.f1_score(vetor_esperado, vetor_resultados)
+    print("Taxa de F1 utilizando:\n{:6.2f}%".format(f1 * 100))
+
+
 # Utilização dos dados de treinamento
 arq = open('suicidas.txt', 'r')
 suicida_texts = tratar_texto(arq)
@@ -208,36 +300,152 @@ treino_suicida, validacao_suicida = dividir_base(suicida_texts)
 treino_nao_suicida, validacao_nao_suicida = dividir_base(nao_suicida_texts)
 
 # Cria a label para o vetor. Esta é a forma que o doc2vec aceita
-# tagged_data = [TaggedDocument(words=linha, tags=['0','NÃO_SUICIDA_'+str(index)]) for index, linha in enumerate(treino_nao_suicida)]
-# tagged_data += [TaggedDocument(words=linha, tags=['1','SUICIDA_'+str(index)]) for index, linha in enumerate(treino_suicida)]
-# #print(tagged_data)
+tagged_data = [TaggedDocument(words=linha, tags=['0','NÃO_SUICIDA_'+str(index)]) for index, linha in enumerate(treino_nao_suicida)]
+tagged_data += [TaggedDocument(words=linha, tags=['1','SUICIDA_'+str(index)]) for index, linha in enumerate(treino_suicida)]
+#print(tagged_data)
 
 
 #Inicializando e treinando modelo
-# model = treinar_modelo(tagged_data)
-# model.save("2d2v.model")
-# print("Model Saved")
+model = treinar_modelo(tagged_data)
+model.save("2d2v.model")
+print("Model Saved")
 
 #carrega o modelo treinamento.
 model= Doc2Vec.load("2d2v.model")
 
 # Gerando classificador
-classificador = gerar_classificador(model, treino_suicida, treino_nao_suicida)
+classificador_reg_logistica     = gerar_classificador_reg_logistica(model, treino_suicida, treino_nao_suicida)
+classificador_random_forest     = gerar_classificador_random_forest(model, treino_suicida, treino_nao_suicida)
+classificador_naive_bayes       = gerar_classificador_naive_bayes(model, treino_suicida, treino_nao_suicida)
+classificador_gradient_boosting = gerar_classificador_gradient_boosting(model, treino_suicida, treino_nao_suicida)
 
 # precisao = aferir_precisao(model, classificador, validacao_suicida, validacao_nao_suicida)
 # print('Taxa de acertos: {:02.2f}'.format(precisao))
 
-# fpr, tpr, thresholds = calc_roc(model, classificador, validacao_suicida, validacao_nao_suicida)
-# auc = metrics.auc(fpr, tpr)
-# print(auc)
-# plot_ROC(fpr, tpr, auc)
+###################### GERAÇÃO DA AUC E CURVA ROC #############################
+
+fpr, tpr, thresholds = calc_roc(model, classificador_reg_logistica, validacao_suicida, validacao_nao_suicida)
+auc_reg_logistica = metrics.auc(fpr, tpr)
+print(auc_reg_logistica)
+plot_ROC(fpr, tpr, auc_reg_logistica)
+
+
+fpr, tpr, thresholds = calc_roc(model, classificador_random_forest, validacao_suicida, validacao_nao_suicida)
+auc_random_forest = metrics.auc(fpr, tpr)
+print(auc_random_forest)
+plot_ROC(fpr, tpr, auc_random_forest)
+
+
+fpr, tpr, thresholds = calc_roc(model, classificador_naive_bayes, validacao_suicida, validacao_nao_suicida)
+auc_naive_bayes = metrics.auc(fpr, tpr)
+print(auc_naive_bayes)
+plot_ROC(fpr, tpr, auc_naive_bayes)
+
+fpr, tpr, thresholds = calc_roc(model, classificador_gradient_boosting, validacao_suicida, validacao_nao_suicida)
+auc_gradient_boosting = metrics.auc(fpr, tpr)
+print(auc_gradient_boosting)
+plot_ROC(fpr, tpr, auc_gradient_boosting)
+
+
+###############################
+#####  Testando Precisão  #####
+###############################
+
+# Vetores de validação: validacao_suicida, validacao_nao_suicida
+
+vetores_frases_suicidas = []
+for frase in validacao_suicida:
+    vetores_frases_suicidas.append(gerar_vetor(model, frase))
+
+vetores_frases_nao_suicidas = []
+for frase in validacao_nao_suicida:
+    vetores_frases_nao_suicidas.append(gerar_vetor(model, frase))
+
+vetor_resultado_esperado = [1 for i in range(len(vetores_frases_suicidas))]
+vetor_resultado_esperado += [0 for i in range(len(vetores_frases_nao_suicidas))]
+
+#Atribuição dos valores aos vetores 
+
+vetor_reg_logistica = []
+for frase in vetores_frases_suicidas:
+    resultado = classificador_reg_logistica.predict_proba([frase])
+    if resultado[0][0] < resultado[0][1]:
+        vetor_reg_logistica.append(1)
+    else:
+        vetor_reg_logistica.append(0)
+for frase in vetores_frases_nao_suicidas:
+    resultado = classificador_reg_logistica.predict_proba([frase])
+    if resultado[0][0] < resultado[0][1]:
+        vetor_reg_logistica.append(1)
+    else:
+        vetor_reg_logistica.append(0)
+
+print("\n\n####  Regressão Logistica  ####")
+print_metrics(vetor_resultado_esperado, vetor_reg_logistica)
+###########################################################################
+
+vetor_random_forest = []
+for frase in vetores_frases_suicidas:
+    resultado = classificador_random_forest.predict_proba([frase])
+    if resultado[0][0] < resultado[0][1]:
+        vetor_random_forest.append(1)
+    else:
+        vetor_random_forest.append(0)
+for frase in vetores_frases_nao_suicidas:
+    resultado = classificador_random_forest.predict_proba([frase])
+    if resultado[0][0] < resultado[0][1]:
+        vetor_random_forest.append(1)
+    else:
+        vetor_random_forest.append(0)
+
+print("\n\n####  Random Forest  ####")
+print_metrics(vetor_resultado_esperado, vetor_random_forest)
+
+#############################################################################
+
+vetor_naive_bayes = []
+for frase in vetores_frases_suicidas:
+    resultado = classificador_naive_bayes.predict_proba([frase])
+    if resultado[0][0] < resultado[0][1]:
+        vetor_naive_bayes.append(1)
+    else:
+        vetor_naive_bayes.append(0)
+for frase in vetores_frases_nao_suicidas:
+    resultado = classificador_naive_bayes.predict_proba([frase])
+    if resultado[0][0] < resultado[0][1]:
+        vetor_naive_bayes.append(1)
+    else:
+        vetor_naive_bayes.append(0)
+
+print("\n\n####  Naive Bayes  ####")
+print_metrics(vetor_resultado_esperado, vetor_naive_bayes)
+
+#############################################################################
+
+vetor_gradient_boosting = []
+for frase in vetores_frases_suicidas:
+    resultado = classificador_gradient_boosting.predict_proba([frase])
+    if resultado[0][0] < resultado[0][1]:
+        vetor_gradient_boosting.append(1)
+    else:
+        vetor_gradient_boosting.append(0)
+for frase in vetores_frases_nao_suicidas:
+    resultado = classificador_gradient_boosting.predict_proba([frase])
+    if resultado[0][0] < resultado[0][1]:
+        vetor_gradient_boosting.append(1)
+    else:
+        vetor_gradient_boosting.append(0)
+
+print("\n\n####  Gradient Boosting  ####")
+print_metrics(vetor_resultado_esperado, vetor_gradient_boosting)
+
 
 
 ############################
 #####  Testando frase  #####
 ############################
 
-while(1):
+while(0):
     frase = input('Favor informar frase a ser testada: ')
     print('\nFrase a ser testada: \"' + frase + '\"')
     frase = tratar_texto([frase])
@@ -246,7 +454,7 @@ while(1):
     vetor = gerar_vetor(model, frase[0])
     print('Vetor gerado a partir da frase:')
     print(vetor)
-    resultado = classificador.predict_proba([vetor])
+    resultado = classificador_reg_logistica.predict_proba([vetor])
     print('\nResultado: ')
     print(' * {:6.2f}% de chance da frase possuir ideação suicida'.format(resultado[0][1]     * 100))
     print(' * {:6.2f}% de chance da frase não possuir ideação suicida'.format(resultado[0][0] * 100))
